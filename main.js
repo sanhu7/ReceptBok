1c1963a5b3d44997cfbdaa4814d38c413f093a49
Sana Naqeeb <sansoonm2012@gmail.com>
2026-04-30 13:04:16 +0200

    visa recept i listan efter submit

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const recipeForm = document.getElementById("recipe-form");
+const recipeList = document.getElementById("recipe-list");
 
 recipeForm.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -7,10 +8,25 @@ recipeForm.addEventListener("submit", function (event) {
     const category = document.getElementById("category").value;
     const description = document.getElementById("description").value;
 
-    const recipe = {
-        title: title,
-        category: category,
-        description: description
-    };
-    console.log(recipe);
+    const recipeCard = document.createElement("article");
+    recipeCard.classList.add("recipe-card");
+
+    const recipeTitle = document.createElement("h3");
+    recipeTitle.textContent = title;
+
+    const recipeCategory = document.createElement("p");
+    recipeCategory.textContent = "Category: " + category;
+
+    const recipeDescription = document.createElement("p");
+    recipeDescription.textContent = description;
+
+    recipeCard.appendChild(recipeTitle);
+    recipeCard.appendChild(recipeCategory);
+    recipeCard.appendChild(recipeDescription);
+
+    recipeList.appendChild(recipeCard);
+
+    recipeForm.reset();
+
+
 })
